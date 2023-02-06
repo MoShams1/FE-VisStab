61e3650e2aeaf169dbfc0dcd34ba92bd32b685d0
Mohammad Shams <m.shamsahmar@gmail.com>
2023-02-06 12:20:09 +0100

    started to add python files with pilot01.py

---
+++ stimulus/pilot01.py
@@ -0,0 +1,113 @@
+"""
+***** Project Frame Effect and Saccade Main Sequence
+***** Pilot Experiment 01
+
+        Mo Shams <MShamsCBR@gmail.com>
+        Initiated on: Feb 06, 2023
+
+A square-shaped object moves along a certain distance according to the typical
+velocity profile of a saccade.
+
+"""
+
+import os
+import random
+import numpy as np
+from lib import config_visual as cvis, genpath, keymouse, timestamp
+
+# ----------------------------------------------------------------------------
+
+# /// GENERAL SETTINGS ///
+
+subID = 'test'
+NTRIALS = 1
+screen_num = 0  # 0: primary    1: secondary
+frame_rate = 120
+full_screen = False
+# ----------------------------------------------------------------------------
+
+# /// CONFIGURE VISUAL OBJECTS ///
+
+# /// frame rate downsampling
+# division by 60 to obtain 60 Hz (16.67 ms per frame) regardless of actual
+# frame rate
+frame_rate_rep = int(frame_rate / 60)
+practical_fr = int(frame_rate / frame_rate_rep)
+
+# /// background
+bg_color = 'black'
+
+# /// temporal gap
+# sec x Hz = frames
+gap_dur_arr = np.round(np.arange(1, 1.5, .1) * practical_fr)
+gap_dur_arr = gap_dur_arr.astype(int)
+
+# /// fixation dot
+fixdot_size = .7
+fixdot_pos = (0, 0)
+fixdot_color = 'white'
+
+# /// moving object
+movobj_size = 5
+movobj_color = 'white'
+movobj_firstpos = (-5, 5)
+movobj_lastpos = (5, 5)  # two potential last positions
+movobj_dur = int(.5 * practical_fr)  # sec x Hz = frames
+movobj_pathx, movobj_pathy = genpath.linear(pos1=movobj_firstpos,
+                                            pos2=movobj_lastpos,
+                                            dur=movobj_dur)
+# ----------------------------------------------------------------------------
+
+# /// CONFIGURE MONITOR ///
+
+mon = cvis.configmon_imac()
+win = cvis.configwin(mon=mon, screen=screen_num,
+                     fullscr=full_screen,
+                     color=bg_color)
+cvis.test_framerate(win=win, nominal_fr=frame_rate)
+# ----------------------------------------------------------------------------
+
+# /// START TRIAL ///
+
+for itrial in range(NTRIALS):
+    # -------------------------------
+
+    # /// set up trial variables
+
+    # decide on gap durations
+    firstgap_dur = np.random.choice(gap_dur_arr)
+    lastgap_dur = np.random.choice(gap_dur_arr)
+
+    # decide on the motion direction and adjust motion path and flash position
+    movobj_dir = random.choice([-1, 1])
+    if movobj_dir == -1:
+        movobj_pathx_tr = -movobj_pathx
+    else:
+        movobj_pathx_tr = movobj_pathx
+    movobj_pathy_tr = movobj_pathy
+    # -------------------------------
+
+    # /// run task
+
+    # gap period
+    for frame in range(firstgap_dur):
+        win.flip()
+
+    # motion period
+    for iframe in range(movobj_dur):
+        for ifrrep in range(frame_rate_rep):
+            cvis.addfixdot(win=win, size=fixdot_size, pos=fixdot_pos,
+                           color=fixdot_color)
+            cvis.addsquare(win=win, width=movobj_size, color=movobj_color,
+                           fillcolor=bg_color,
+                           pos=(movobj_pathx_tr[iframe],
+                                movobj_pathy_tr[iframe]),
+                           line_width=.1)
+            win.flip()
+
+    # gap period
+    for frame in range(lastgap_dur):
+        win.flip()
+    # -------------------------------
+
+win.close()
