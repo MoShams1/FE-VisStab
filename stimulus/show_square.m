008ae7f6168a01a587eba5fc72c5e2d2f017f9db
Mohammad Shams <m.shamsahmar@gmail.com>
2023-07-04 16:57:15 +0200

    a grating moves back and forth

--- stimulus/show_square.m
+++ stimulus/show_square.m
@@ -1,31 +1,75 @@
-clc, clear, close all
+clc;
+clear;
+close all;
 
-% skip syncronization test for mac
+% skip synchronization test for Mac
 Screen('Preference', 'SkipSyncTests', 1);
 
-% create window
+% set window parameters
 screen_number = 0;
-bg_color = [255 255 255] ./ 2;
+bg_color = [1 1 1]*255 / 2;
 [win, dim] = Screen('OpenWindow', screen_number, bg_color);
 ox = dim(3)/2;
 oy = dim(4)/2;
 
-% create text
+% set motion parameters
+ref_rate = 120;
+speed = 2;  % dva per sec
+phase_vec_base = linspace(0,180,speed*ref_rate);
+phase_vec = [phase_vec_base(1:end-1), fliplr(phase_vec_base(2:end))];
+
+% set grating parameters
+ncyc = 3;
+gr_width = dva2pix(speed * 2 * ncyc);
+gr_height = dva2pix(10);
+freq = ncyc/gr_width;
+contrast = 5;
+squareRect = [-gr_width/2, -gr_height/2, gr_width/2, gr_height/2] + [ox,oy,ox,oy];
+
+% set probe parameters
+probe_d = dva2pix(1);
+probeRect = [-probe_d/2,-probe_d/2,probe_d/2,probe_d/2]+[ox,oy,ox,oy];
+
+% set fixation mark parameters
 text_color = [0 0 0];
-text_x = 'center';
 text_y = 'center';
-DrawFormattedText(win, '+', text_x, text_y, text_color);
+text_x = dva2pix(20);
 
-% create square
-squareSize = 150;
-squareWidth = 5;
-squareColor = [0 0 0];
-squareRect = [-squareSize, -squareSize, squareSize, squareSize] + [ox, oy, ox, oy];
-Screen('FrameRect', win, squareColor, squareRect, squareWidth);
+iphase = 0;
+for irep = 1:2
+    for iphase = phase_vec
 
-HideCursor();
-Screen('Flip', win);
+        % add fixation mark
+        DrawFormattedText(win, '+', text_x, text_y, text_color);
 
-WaitSecs(3);
+        % add grating
+        phase = iphase;
+        gabor = CreateProceduralSineGrating(win, gr_width, gr_height, [0,0,0,0]);
+        Screen('DrawTexture', win, gabor, [], squareRect, [], [], [], [], [], [], ...
+            [phase, freq, contrast, 0]);
+
+        if iphase == 0
+            % flash red probe
+            Screen('FillOval', win, [1,0,0]*255, probeRect)
+        elseif iphase == 180
+            % flash blue probe
+            Screen('FillOval', win, [0,0,1]*255, probeRect)
+        end
+
+        HideCursor();
+        Screen('Flip', win);
+
+%         WaitSecs(.1);
+
+    end
+end
 
 Screen('CloseAll');
+
+
+function pix = dva2pix(theta)
+mon_width = 30.41;  % cm
+mon_dist = 70;  % cm
+pix_per_deg = atan(mon_width/(2*mon_dist)) * 2 * 180 / pi;
+pix = round(theta*pix_per_deg);
+end
